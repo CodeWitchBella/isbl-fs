6ad11bc3f4966cb4601a1da6e9cbbff1fdd26361
Isabella Skořepová <isabella@skorepova.info>
2021-09-16 21:12:35 +0200

    feat: export all non-deprecated fs/promises methods

--- index.js
+++ index.js
@@ -3,28 +3,69 @@ import * as fsPromises from "fs/promises"
 // Adding new function:
 // 1. add `export const ...` here
 // 2. add it to `export default { ... }`
-export const readFile = wrap("readFile")
-export const writeFile = wrap("writeFile")
-export const mkdir = wrap("mkdir")
+export const access = wrap("access")
+export const appendFile = wrap("appendFile")
+export const chmod = wrap("chmod")
+export const chown = wrap("chown")
 export const copyFile = wrap("copyFile")
-export const rm = wrap("rm")
+export const cp = wrap("cp")
+// lchmod is deprecated, therefore I am not reexporting it
+export const lchown = wrap("lchown")
+export const lutimes = wrap("lutimes")
+export const link = wrap("link")
+export const lstat = wrap("lstat")
+export const mkdir = wrap("mkdir")
+export const mkdtemp = wrap("mkdtemp")
 export const open = wrap("open")
 export const opendir = wrap("opendir")
 export const readdir = wrap("readdir")
+export const readFile = wrap("readFile")
+export const readlink = wrap("readlink")
+export const realpath = wrap("realpath")
+export const rename = wrap("rename")
+export const rmdir = wrap("rmdir")
+export const rm = wrap("rm")
+export const stat = wrap("stat")
+export const symlink = wrap("symlink")
+export const truncate = wrap("truncate")
+export const unlink = wrap("unlink")
+export const utimes = wrap("utimes")
+export const watch = wrap("watch")
+export const writeFile = wrap("writeFile")
 export default {
-  readFile,
-  writeFile,
-  mkdir,
+  access,
+  appendFile,
+  chmod,
+  chown,
   copyFile,
-  rm,
+  cp,
+  lchown,
+  lutimes,
+  link,
+  lstat,
+  mkdir,
+  mkdtemp,
   open,
   opendir,
   readdir,
+  readFile,
+  readlink,
+  realpath,
+  rename,
+  rmdir,
+  rm,
+  stat,
+  symlink,
+  truncate,
+  unlink,
+  utimes,
+  watch,
+  writeFile,
 }
 
 /** @type {<M extends keyof typeof fsPromises>(method: M) => fsPromises[M]} */
 function wrap(method) {
-  if (!fsPromises[method]) return null
+  if (!fsPromises[method]) return undefined
   Object.defineProperty(wrapper, "name", { writable: true })
   wrapper.name = fsPromises[method].name
   Object.defineProperty(wrapper, "name", { writable: false })
